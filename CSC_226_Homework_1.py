8bea10f9610d4ddc6430ff48f8e181ac36b0a1f8
Carl Gordon <carl.gord2022@gmail.com>
2025-02-25 10:59:33 -0500

    Create CSC_226_Homework_1.py
    Python Homework 1 CSC 226

---
+++ CSC_226_Homework_1.py
@@ -0,0 +1,10 @@
+## Create a function and loop in python
+
+def countdown(number):
+    while number>0:
+        print(f"Countdown:{number}")
+        number-=1
+            
+##Call the function
+countdown(10)   
+print ("Happy New Year!")
